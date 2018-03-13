eaa5e2fc160c7ec6f40d6a90b27b1893e65b02b4
yzx <369943318@qq.com>
2018-03-13 21:01:08 +0800

    first commit

--- djangotest/books/views.py
+++ djangotest/books/views.py
@@ -10,6 +10,19 @@ class IndexView(ListView):
     model = Book
     template_name = 'index.html'
     context_object_name = 'booklist'
+	
+	paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        # 先调用父类的方法，获取默认的context
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        start, end = utils.custompaginator(paginator.num_pages, page.number, 3)
+        context.update({
+            'page_range': range(start, end + 1)
+        })
+        return context
 
 class BookListView(ListView):
     model = Book
